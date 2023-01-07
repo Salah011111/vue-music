74f8322c2812336c7a7ad17c9c7b7064b664e48a
Salah <2236291956@qq.com>
2023-01-07 23:58:54 +0800

    'init'

---
+++ src/main/java/com/javaclimb/music/config/WebMvcConfig.java
@@ -0,0 +1,26 @@
+package com.javaclimb.music.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * 解决跨域问题
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowCredentials(true);
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/images/");
+        super.addResourceHandlers(registry);
+    }
+}
